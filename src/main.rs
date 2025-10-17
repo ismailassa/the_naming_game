f513b4e2518011af8aba84513b11dc92f755a862
ismailassa <ismailassakali20@gmail.com>
2025-10-17 20:22:45 +0200

    add context selection
    add agent selection

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,29 @@ use rand::Rng;
 
 fn main() {
     println!("Start the Naming Game simulation!");
-    let world = World::new(10);
-    let population = Population::new(10);
+    let world_and_population_size: u32 = 10;
+    let world = World::new(world_and_population_size);
+    let population = Population::new(world_and_population_size);
     // println!("World: {:?}", world);
     // println!("Population: {:?}", population);
 
     // Context selection, consists of 5 objects
-    let context_size = 5;
-    let x = generate_random_numbers(context_size);
-    println!("x: {:?}", x);
+    let context_size: usize = 5;
+    let context: Vec<u32> = get_random_elements(&world.objects, context_size);
+
+    println!("Context selected (object IDs): {:?}", context);
 }
 
-fn generate_random_numbers(amount: usize) -> Vec<u32> {
-    let mut numbers = Vec::new();
+fn get_random_elements<T: Clone + PartialEq>(elements: &Vec<T>, amount: usize) -> Vec<T> {
+    let mut selected_elements = Vec::new();
     let mut rng = rand::rng();
-    while numbers.len() < amount {
-        let random_num = rng.random_range(0..10);
-        if (numbers.contains(&random_num)) {
+    while selected_elements.len() < amount {
+        let random_index = rng.random_range(0..elements.len());
+        let element = elements[random_index].clone();
+        if selected_elements.contains(&element) {
             continue;
         }
-        numbers.push(random_num);
+        selected_elements.push(element);
     }
-    numbers
+    selected_elements
 }
