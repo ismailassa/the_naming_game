252e3943ececd347da146689cad15f08bfeac6e7
ismailassa <ismailassakali20@gmail.com>
2025-10-17 17:06:58 +0200

    test current imp

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,6 @@ fn main() {
     println!("Start the Naming Game simulation!");
     let world = World::new(10);
     let population = Population::new(10);
+    println!("World: {:?}", world);
+    println!("Population: {:?}", population);
 }
